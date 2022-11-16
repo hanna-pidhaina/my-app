1a59819c4901eff4cabf8a714b6c146668abe9d6
hanna-pidhaina <109957147+hanna-pidhaina@users.noreply.github.com>
2022-11-16 15:44:53 +0200

    Deleted unused elements

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import { useState, CSSProperties } from "react";
 import CircleLoader from "react-spinners/CircleLoader";
 
 export default function Weather(props) {
