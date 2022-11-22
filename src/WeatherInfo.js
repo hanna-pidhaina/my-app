31906ad379c05df716edc6c0b78317f2fc228ab2
hanna-pidhaina <109957147+hanna-pidhaina@users.noreply.github.com>
2022-11-22 13:44:01 +0200

    Add images and variable for images

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-import Cloud from "./Cloud.jpg";
+import cloud from "./cloud.jpg";
+import cloudBolt from "./cloudBolt.jpg";
+import cloudMoon from "./cloudMoon.jpg";
+import cloudMoonRain from "./cloudMoonRain.jpg";
+import cloudRain from "./cloudRain.jpg";
+import cloudSun from "./cloudSun.jpg";
+import cloudSunRain from "./cloudSunRain.jpg";
+import moon from "./moon.jpg";
+import smog from "./smog.jpg";
+import snowflake from "./snowflake.jpg";
+import sun from "./sun.jpg";
 import ForecastDay from "./ForecastDay";
 
 import ReactDOM from "react-dom";
@@ -10,7 +20,7 @@ import {
   faSnowflake,
   faMoon,
   faSun,
-  faSUnCloud,
+  faCloudSun,
   faCloudMoon,
   faCloudRain,
   faCloudSunRain,
@@ -25,35 +35,57 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 export default function WeatherInfo(props) {
   let iconNum = props.data.icon;
   let iconMapping = {
-    "01d": "Sun",
-    "01n": "Moon",
-    "02d": "CloudSun",
-    "02n": "CloudMoon",
-    "03d": "Cloud",
-    "03n": "Cloud",
-    "04d": "Cloud",
-    "04n": "Cloud",
-    "09d": "CloudRain",
-    "09n": "CloudRain",
-    "10d": "CloudSunRain",
-    "10n": "CloudMoonRain",
-    "11d": "CloudBolt",
-    "11n": "CloudBolt",
-    "13d": "Snowflake",
-    "13n": "Snowflake",
-    "50d": "Smog",
-    "50n": "Smog",
+    "01d": "faSun",
+    "01n": "faMoon",
+    "02d": "faCloudSun",
+    "02n": "faCloudMoon",
+    "03d": "faCloud",
+    "03n": "faCloud",
+    "04d": "faCloud",
+    "04n": "faCloud",
+    "09d": "faCloudRain",
+    "09n": "faCloudRain",
+    "10d": "faCloudSunRain",
+    "10n": "faCloudMoonRain",
+    "11d": "faCloudBolt",
+    "11n": "faCloudBolt",
+    "13d": "faSnowflake",
+    "13n": "faSnowflake",
+    "50d": "faSmog",
+    "50n": "faSmog",
+  };
+  let imgMapping = {
+    "faSun": "sun",
+    "faMoon": "moon",
+    "faCloudSun": "cloudSun",
+    "faCloudMoon": "cloudMoon",
+    "faCloud": "cloud",
+    "faCloud": "cloud",
+    "faCloud": "cloud",
+    "faCloud": "cloud",
+    "faCloudRain": "cloudRain",
+    "faCloudRain": "cloudRain",
+    "faCloudSunRain": "cloudSunRain",
+    "faCloudMoonRain": "cloudSunRain",
+    "faCloudBolt": "cloudBolt",
+    "faCloudBolt": "cloudBolt",
+    "faSnowflake": "snowflake",
+    "faSnowflake": "snowflake",
+    "faSmog": "smog",
+    "faSmog": "smog",
   };
   let iconName = iconMapping[iconNum];
-  let weatherIcon = <FontAwesomeIcon icon={`fa${iconName}`} />;
-  let weatherIconCheck = <FontAwesomeIcon icon={faCloud} />;
+  let imgName = imgMapping[iconName];
+  let weatherIcon = <FontAwesomeIcon icon={iconName} />;
+  console.log (iconName);
+  console.log (imgName);
 
   return (
     <div className="WeatherInfo">
       <div className="CurrentWeatherSection text-center">
         <div className="row align-items-center justify-content-around">
           <div className="col-md-3">
-            <div className="weather-icon">{weatherIcon} {weatherIconCheck}</div>
+            <div className="weather-icon">{weatherIcon} </div>
           </div>
           <div className="col-md-3">
             <h3>{props.data.city}</h3>
@@ -73,9 +105,9 @@ export default function WeatherInfo(props) {
           <div className="col-md-6">
             <div>
               <img
-                src={iconName}
+                src={imgName}
                 className="weather-img img-fluid"
-                alt={iconName}
+                alt={imgName}
               />
             </div>
           </div>
